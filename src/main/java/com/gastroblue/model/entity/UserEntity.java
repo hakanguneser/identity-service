52bae74460accfaaf1f9de39c63afb843eab1fea
Hakan <hakan.guneser@gmail.com>
2026-04-12 23:07:25 +0300

    JPA changes

--- src/main/java/com/gastroblue/model/entity/UserEntity.java
+++ src/main/java/com/gastroblue/model/entity/UserEntity.java
@@ -28,10 +28,10 @@ import org.springframework.security.core.userdetails.UserDetails;
     })
 public class UserEntity extends Auditable implements UserDetails {
 
-  @Column(name = "COMPANY_ID", nullable = false, length = 36)
+  @Column(name = "COMPANY_ID", length = 36)
   private String companyId;
 
-  @Column(name = "COMPANY_GROUP_ID", nullable = false, length = 36)
+  @Column(name = "COMPANY_GROUP_ID",length = 36)
   private String companyGroupId;
 
   @Column(name = "USERNAME", length = 100)
