cad12871a4d121d3d30212f86eb6d6ee6d65746b
Hakan <hakan.guneser@gmail.com>
2026-04-13 22:51:32 +0300

    Update TrackerController.java

--- src/main/java/com/gastroblue/controller/TrackerController.java
+++ src/main/java/com/gastroblue/controller/TrackerController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/tracker")
+@RequestMapping("/api/v1/tracker")
 @RequiredArgsConstructor
 public class TrackerController {
 
