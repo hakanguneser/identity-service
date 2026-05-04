7653542c70f53134070d3ceaded2ed8dc2a122ee
Hakan <hakan.guneser@gmail.com>
2026-05-04 22:02:05 +0300

    Update Lookups.java

--- src/main/java/com/gastroblue/model/enums/Lookups.java
+++ src/main/java/com/gastroblue/model/enums/Lookups.java
@@ -1,27 +1,35 @@
 package com.gastroblue.model.enums;
 
 import com.gastroblue.commons.helper.lookup.model.base.BaseLookup;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
 public enum Lookups implements BaseLookup {
-  ZONE(true, false, false, "ZONE"),
-  CITY(true, false, true, "CITY"),
-  COUNTRY(true, false, false, "COUNTRY"),
-  SEGMENT_1(true, false, false, "SEGMENT_1"),
-  SEGMENT_2(true, false, false, "SEGMENT_2"),
-  SEGMENT_3(true, false, false, "SEGMENT_3"),
-  SEGMENT_4(true, false, false, "SEGMENT_4"),
-  SEGMENT_5(true, false, false, "SEGMENT_5"),
-  DEPARTMENT(true, true, false, "DEPARTMENT"),
-  LANGUAGE(false, true, false, "LANGUAGE"),
-  GENDER(false, false, false, "GENDER"),
-  APPLICATION_ROLE(false, true, false, "APPLICATION_ROLE");
+  ZONE(true, false, false),
+  CITY(true, false, true),
+  COUNTRY(true, false, false),
+  SEGMENT_1(true, false, false),
+  SEGMENT_2(true, false, false),
+  SEGMENT_3(true, false, false),
+  SEGMENT_4(true, false, false),
+  SEGMENT_5(true, false, false),
+  DEPARTMENT(true, true, false),
+  LANGUAGE(false, true, false),
+  GENDER(false, false, false),
+  APPLICATION_ROLE(false, true, false);
 
   private final boolean companyGroupScope;
   private final boolean productScope;
   private final boolean parentNeeded;
-  private final String category;
+
+  Lookups(boolean companyGroupScope, boolean productScope, boolean parentNeeded) {
+    this.companyGroupScope = companyGroupScope;
+    this.productScope = productScope;
+    this.parentNeeded = parentNeeded;
+  }
+
+  @Override
+  public String getCategory() {
+    return name();
+  }
 }
